8b2014bc4268a4ab207f8e30176ef5f8aab3acf8
wigsters52 <ryanwiggins52@gmail.com>
2021-03-02 15:23:04 -0500

    added partial templates

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,19 +1,44 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
+
+// Define paths
 const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+//Setup handlebars engine
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
 
 app.use(express.static(publicDirectoryPath))
 
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Ryan Wiggins'
+    })
+})
 
-// app.get('/help', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../public/help.html'))
-// })
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About',
+        name: 'Ryan Wiggins'
+    })
+})
 
-// app.get('/about', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../public/about.html'))
-// })
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is some helpful text',
+        title: 'Help',
+        name: 'Ryan Wiggins'
+
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
