0768bd4321a4712e2da46841e96efa9d57d4f01b
wigsters52 <ryanwiggins52@gmail.com>
2021-03-05 11:14:23 -0500

    added geocode to webserver and default values

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
+const geoCode = require('./utlis/geocode')
+const forecast = require('./utlis/forecast')
 
 const app = express()
 
@@ -41,10 +43,41 @@ app.get('/help', (req, res) => {
 })
 
 app.get('/weather', (req, res) => {
-    res.send({
-        "Forecast": "Cloudy and snowing",
-        "Location": "New York, NY"
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide an address'
+        })
+    }
+    geoCode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({ error })
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
     })
+})
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+    console.log(req.query.search)
+    res.send({
+        products: []
+    })
+
+
 })
 
 app.get('/help/*', (req, res) => {
